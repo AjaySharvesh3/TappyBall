309c51938fb50d56b77a708d7ca9f5b5dfb2f96e
AjaySharvesh3 <ajaysharvesh.mp@gmail.com>
2018-04-06 19:41:04 +0530

    TappyBall Game

---
+++ Tappy Ball/Assets/MenuUiManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuUiManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public  void Play() {
+		SceneManager.LoadScene ("level1");
+	}
+}
