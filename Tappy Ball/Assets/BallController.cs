e9f83609060b7b5ceaa89568c7574881747f98bc
AjaySharvesh3 <ajaysharvesh.mp@gmail.com>
2018-04-08 09:47:59 +0530

    Updated Project.
    Final touch with little bugs. Failed gradle to build.

---
+++ Tappy Ball/Assets/BallController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+	Rigidbody2D rb;
+	public float upForce;
+	bool started;
+	bool gameOver;
+    public float rotation;
+
+	// Use this for initialization
+	void Start () {
+		
+		rb = GetComponent<Rigidbody2D> ();
+		started = false;
+		gameOver = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (!started) {
+		
+			if (Input.GetMouseButtonDown (0)) {
+
+				started = true;
+				rb.isKinematic = false;
+                GameManager.instance.GameStart();
+
+			} 
+
+		}
+		else if (started && !gameOver) {
+
+            transform.Rotate(0, 0, rotation);
+
+			if (Input.GetMouseButtonDown (0)) {
+
+				rb.velocity = Vector2.zero;
+				rb.AddForce (new Vector2 (0, upForce));
+				//rb.AddForce(Vector2.up * upForce);
+
+			}
+
+		}
+	}
+
+    void OnCollisionEnter2D(Collision2D col) {
+        gameOver = true;
+        GameManager.instance.GameOver();
+
+        GetComponent<Animator>().Play("minions");
+    }
+
+	void OnTriggerEnter2D(Collider2D col) {
+
+		if (col.gameObject.tag == "Pipe" && !gameOver) {
+			gameOver = true;
+            GameManager.instance.GameOver();
+
+            GetComponent<Animator>().Play("minions");
+		}
+
+		if (col.gameObject.tag == "ScoreChecker" && !gameOver) {
+			ScoreManager.instance.IncrementScore ();
+		}
+	}
+}
---
+++ Tappy Ball/Assets/UiManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UiManager : MonoBehaviour {
+
+	public static UiManager instance;
+    public GameObject gameOverPanel;
+    public GameObject startUI;
+    public GameObject gameOverText;
+    public Text scoreText;
+    public Text highScoreText;
+
+	void Awake() {
+		if (instance == null) {
+			instance = this;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		scoreText.text = ScoreManager.instance.score.ToString ();
+	}
+
+    public void GameStart() {
+        startUI.SetActive(false);
+    }
+
+    public void GameOver() {
+        gameOverText.SetActive(true);
+        highScoreText.text = "HIGH SCORE : " + PlayerPrefs.GetInt("HighScore");
+        gameOverPanel.SetActive(true);
+    }
+
+    public void Menu() {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void replay() {
+        SceneManager.LoadScene("level1");
+    }
+}
